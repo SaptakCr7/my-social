170c4c3d715291f20c72bcf66a10620199d83c1b
SaptakCr7 <saptaksinha13898@gmail.com>
2022-12-10 18:07:58 +0530

    Major Changes

---
+++ src/components/PostApi.vue
@@ -0,0 +1,20 @@
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Employee List",
+
+  mounted() {
+    Vue.axios.get("https://jsonplaceholder.typicode.com/posts");
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Employee List</h1>
+  </div>
+</template>
